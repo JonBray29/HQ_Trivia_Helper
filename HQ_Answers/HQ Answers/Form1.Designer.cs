75e0b52fbbc23678ad3358966eac7ca6ac78d173
Jonathon <jonathon.bray@students.plymouth.ac.uk>
2018-11-15 12:24:28 +0000

    Working HQ Trivia answer bot

---
+++ HQ_Answers/HQ Answers/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace HQ_Answers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAnswer1 = new System.Windows.Forms.TextBox();
+            this.txtAnswer2 = new System.Windows.Forms.TextBox();
+            this.txtAnswer3 = new System.Windows.Forms.TextBox();
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.lblCount1 = new System.Windows.Forms.Label();
+            this.lblCount2 = new System.Windows.Forms.Label();
+            this.lblCount3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAnswer1
+            // 
+            this.txtAnswer1.BackColor = System.Drawing.Color.White;
+            this.txtAnswer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.txtAnswer1.Location = new System.Drawing.Point(61, 375);
+            this.txtAnswer1.Name = "txtAnswer1";
+            this.txtAnswer1.ReadOnly = true;
+            this.txtAnswer1.Size = new System.Drawing.Size(381, 45);
+            this.txtAnswer1.TabIndex = 0;
+            this.txtAnswer1.Text = "Answer 1";
+            this.txtAnswer1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtAnswer2
+            // 
+            this.txtAnswer2.BackColor = System.Drawing.Color.White;
+            this.txtAnswer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.txtAnswer2.Location = new System.Drawing.Point(62, 426);
+            this.txtAnswer2.Name = "txtAnswer2";
+            this.txtAnswer2.ReadOnly = true;
+            this.txtAnswer2.Size = new System.Drawing.Size(381, 45);
+            this.txtAnswer2.TabIndex = 1;
+            this.txtAnswer2.Text = "Answer 2";
+            this.txtAnswer2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtAnswer3
+            // 
+            this.txtAnswer3.BackColor = System.Drawing.Color.White;
+            this.txtAnswer3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.txtAnswer3.Location = new System.Drawing.Point(61, 477);
+            this.txtAnswer3.Name = "txtAnswer3";
+            this.txtAnswer3.ReadOnly = true;
+            this.txtAnswer3.Size = new System.Drawing.Size(381, 45);
+            this.txtAnswer3.TabIndex = 2;
+            this.txtAnswer3.Text = "Answer 3";
+            this.txtAnswer3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.AcceptsReturn = true;
+            this.txtQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.txtQuestion.Location = new System.Drawing.Point(61, 107);
+            this.txtQuestion.Multiline = true;
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.ReadOnly = true;
+            this.txtQuestion.Size = new System.Drawing.Size(380, 262);
+            this.txtQuestion.TabIndex = 3;
+            this.txtQuestion.Text = "Question";
+            this.txtQuestion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnFind
+            // 
+            this.btnFind.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(158)))), ((int)(((byte)(255)))));
+            this.btnFind.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.btnFind.Location = new System.Drawing.Point(60, 12);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(381, 89);
+            this.btnFind.TabIndex = 4;
+            this.btnFind.Text = "FIND ANSWER!!!";
+            this.btnFind.UseVisualStyleBackColor = false;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // lblCount1
+            // 
+            this.lblCount1.AutoSize = true;
+            this.lblCount1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.lblCount1.Location = new System.Drawing.Point(446, 378);
+            this.lblCount1.Name = "lblCount1";
+            this.lblCount1.Size = new System.Drawing.Size(36, 39);
+            this.lblCount1.TabIndex = 5;
+            this.lblCount1.Text = "0";
+            // 
+            // lblCount2
+            // 
+            this.lblCount2.AutoSize = true;
+            this.lblCount2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.lblCount2.Location = new System.Drawing.Point(446, 429);
+            this.lblCount2.Name = "lblCount2";
+            this.lblCount2.Size = new System.Drawing.Size(36, 39);
+            this.lblCount2.TabIndex = 6;
+            this.lblCount2.Text = "0";
+            // 
+            // lblCount3
+            // 
+            this.lblCount3.AutoSize = true;
+            this.lblCount3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.lblCount3.Location = new System.Drawing.Point(446, 480);
+            this.lblCount3.Name = "lblCount3";
+            this.lblCount3.Size = new System.Drawing.Size(36, 39);
+            this.lblCount3.TabIndex = 7;
+            this.lblCount3.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(513, 534);
+            this.Controls.Add(this.lblCount3);
+            this.Controls.Add(this.lblCount2);
+            this.Controls.Add(this.lblCount1);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtQuestion);
+            this.Controls.Add(this.txtAnswer3);
+            this.Controls.Add(this.txtAnswer2);
+            this.Controls.Add(this.txtAnswer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAnswer1;
+        private System.Windows.Forms.TextBox txtAnswer2;
+        private System.Windows.Forms.TextBox txtAnswer3;
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label lblCount1;
+        private System.Windows.Forms.Label lblCount2;
+        private System.Windows.Forms.Label lblCount3;
+    }
+}
+
---
+++ HQ_Answers/HQ Answers/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing;
+using Google.Cloud.Vision.V1;
+using Google.Apis.Auth.OAuth2;
+using Grpc.Auth;
+using System.IO;
+using System.Text.RegularExpressions;
+using GoogleCSE;
+
+namespace HQ_Answers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string question;
+        private string answer1;
+        private string answer2;
+        private string answer3;
+        private int count1;
+        private int count2;
+        private int count3;
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            resetColours();
+            captureQuestion();
+            captureAnswer1();
+            captureAnswer2();
+            captureAnswer3();
+            readQuestion();
+            readAnswer1();
+            readAnswer2();
+            readAnswer3();
+            showQuestion();
+            showAnswer1();
+            showAnswer2();
+            showAnswer3();
+            searchQuestion();
+            searchAnswer1();
+            searchAnswer2();
+            searchAnswer3();
+            changeColours();
+        }
+
+        //Reset text box colours --------------------------------------------------------------------------------------------------------------------
+        private void resetColours()
+        {
+            txtAnswer1.BackColor = Color.White;
+            txtAnswer2.BackColor = Color.White;
+            txtAnswer3.BackColor = Color.White;
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------------
+
+        //Capture Screenshots -----------------------------------------------------------------------------------------------------------------------
+        private void captureQuestion()
+        {
+            Rectangle rect = new Rectangle(760, 230, 1150, 395);
+            Bitmap bmp = new Bitmap(390, 165, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            bmp.Save(@"E:\Projects\HQ Answers\Capture\Question.JPG", ImageFormat.Jpeg);
+        }
+        private void captureAnswer1()
+        {
+            Rectangle rect = new Rectangle(760, 395, 1150, 460);
+            Bitmap bmp = new Bitmap(390, 65, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            bmp.Save(@"E:\Projects\HQ Answers\Capture\A1.JPG", ImageFormat.Jpeg);
+        }
+        private void captureAnswer2()
+        {
+            Rectangle rect = new Rectangle(760, 460, 1150, 530);
+            Bitmap bmp = new Bitmap(390, 70, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            bmp.Save(@"E:\Projects\HQ Answers\Capture\A2.JPG", ImageFormat.Jpeg);
+        }
+        private void captureAnswer3()
+        {
+            Rectangle rect = new Rectangle(760, 530, 1150, 600);
+            Bitmap bmp = new Bitmap(390, 70, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(rect.Left, rect.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+            bmp.Save(@"E:\Projects\HQ Answers\Capture\A3.JPG", ImageFormat.Jpeg);
+        }
+        //End Capture screenshots -------------------------------------------------------------------------------------------------------------------
+        
+        //Use Google Vision to Extract Text From Screenshots ----------------------------------------------------------------------------------------
+        private void readQuestion()
+        {
+            string jsonPath = @"E:\Projects\My Project 11722-a87347b406c3.json";
+            var credential = GoogleCredential.FromFile(jsonPath).CreateScoped(ImageAnnotatorClient.DefaultScopes);
+            var channel = new Grpc.Core.Channel(ImageAnnotatorClient.DefaultEndpoint.ToString(), credential.ToChannelCredentials());
+            var client = ImageAnnotatorClient.Create(channel);
+            var image = Google.Cloud.Vision.V1.Image.FromFile(@"E:\Projects\HQ Answers\Capture\Question.JPG");
+            var response = client.DetectText(image);
+            StreamWriter write = new StreamWriter(@"E:\Projects\question.txt");
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                {
+                    string result = Regex.Replace(annotation.Description, @"\r\n?|\n", " ");
+                    write.WriteLine(result);
+                }
+            }
+            write.Close();
+        }
+        private void readAnswer1()
+        {
+            string jsonPath = @"E:\Projects\My Project 11722-a87347b406c3.json";
+            var credential = GoogleCredential.FromFile(jsonPath).CreateScoped(ImageAnnotatorClient.DefaultScopes);
+            var channel = new Grpc.Core.Channel(ImageAnnotatorClient.DefaultEndpoint.ToString(), credential.ToChannelCredentials());
+            var client = ImageAnnotatorClient.Create(channel);
+            var image = Google.Cloud.Vision.V1.Image.FromFile(@"E:\Projects\HQ Answers\Capture\A1.JPG");
+            var response = client.DetectText(image);
+            StreamWriter write = new StreamWriter(@"E:\Projects\A1.txt");
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    write.WriteLine(annotation.Description);
+            }
+            write.Close();
+        }
+        private void readAnswer2()
+        {
+            string jsonPath = @"E:\Projects\My Project 11722-a87347b406c3.json";
+            var credential = GoogleCredential.FromFile(jsonPath).CreateScoped(ImageAnnotatorClient.DefaultScopes);
+            var channel = new Grpc.Core.Channel(ImageAnnotatorClient.DefaultEndpoint.ToString(), credential.ToChannelCredentials());
+            var client = ImageAnnotatorClient.Create(channel);
+            var image = Google.Cloud.Vision.V1.Image.FromFile(@"E:\Projects\HQ Answers\Capture\A2.JPG");
+            var response = client.DetectText(image);
+            StreamWriter write = new StreamWriter(@"E:\Projects\A2.txt");
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    write.WriteLine(annotation.Description);
+            }
+            write.Close();
+        }
+        private void readAnswer3()
+        {
+            string jsonPath = @"E:\Projects\My Project 11722-a87347b406c3.json";
+            var credential = GoogleCredential.FromFile(jsonPath).CreateScoped(ImageAnnotatorClient.DefaultScopes);
+            var channel = new Grpc.Core.Channel(ImageAnnotatorClient.DefaultEndpoint.ToString(), credential.ToChannelCredentials());
+            var client = ImageAnnotatorClient.Create(channel);
+            var image = Google.Cloud.Vision.V1.Image.FromFile(@"E:\Projects\HQ Answers\Capture\A3.JPG");
+            var response = client.DetectText(image);
+            StreamWriter write = new StreamWriter(@"E:\Projects\A3.txt");
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    write.WriteLine(annotation.Description);
+            }
+            write.Close();
+        }
+        //Finish Extracting Text Also Place Them Into Seperate .txt Files ---------------------------------------------------------------------------
+
+        //Show the Questions and Answers in Text Boxes ----------------------------------------------------------------------------------------------
+        private void showQuestion()
+        {
+            StreamReader read = new StreamReader(@"E:\Projects\question.txt");
+            question = read.ReadLine();
+            txtQuestion.Text = question;
+            read.Close();
+        }
+        private void showAnswer1()
+        {
+            StreamReader read = new StreamReader(@"E:\Projects\A1.txt");
+            answer1 = read.ReadLine();
+            txtAnswer1.Text = answer1;
+            read.Close();
+        }
+        private void showAnswer2()
+        {
+            StreamReader read = new StreamReader(@"E:\Projects\A2.txt");
+            answer2 = read.ReadLine();
+            txtAnswer2.Text = answer2;
+            read.Close();
+        }
+        private void showAnswer3()
+        {
+            StreamReader read = new StreamReader(@"E:\Projects\A3.txt");
+            answer3 = read.ReadLine();
+            txtAnswer3.Text = answer3;
+            read.Close();
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------------
+
+        //Search the Question Using a Custom Search Engine ------------------------------------------------------------------------------------------
+        private void searchQuestion()
+        {
+            var gs = new GoogleSearch("006096622735044125412:9uygw__isym", maxPages: 1, pageSize: 10, key: "AIzaSyBGgsiVhWkoQ_Q8SvRBCAn5I_7cg7O4yWA");
+            var results = gs.Search(question);
+            StreamWriter write = new StreamWriter(@"E:\Projects\Results.txt");
+            foreach (var result in results)
+            {
+                if (result.Description != null)
+                {
+                    string description = result.Description.ToLower();
+                    string content = Regex.Replace(description, @"</b>|<b>|<br>", " ");
+                    write.WriteLine(content);
+                }
+            }
+            write.Close();
+        }
+        //Output All Descriptions Into a txt File ---------------------------------------------------------------------------------------------------
+
+        //Find Answer in txt ------------------------------------------------------------------------------------------------------------------------
+        private void searchAnswer1()
+        {
+            string answer = answer1.ToLower();
+            string text = File.ReadAllText(@"E:\Projects\Results.txt").ToLower();
+            count1 = Regex.Matches(text, answer).Count;
+            lblCount1.Text = Convert.ToString(count1);
+        }
+        private void searchAnswer2()
+        {
+            string answer = answer2.ToLower();
+            string text = File.ReadAllText(@"E:\Projects\Results.txt").ToLower();
+            count2 = Regex.Matches(text, answer).Count;
+            lblCount2.Text = Convert.ToString(count2);
+        }
+        private void searchAnswer3()
+        {
+            string answer = answer3.ToLower();
+            string text = File.ReadAllText(@"E:\Projects\Results.txt").ToLower();
+            count3 = Regex.Matches(text, answer).Count;
+            lblCount3.Text = Convert.ToString(count3);
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------------
+
+        //Using If Statements To Determine Which Count is Greater -----------------------------------------------------------------------------------
+        private void changeColours()
+        {
+            if (count1 > count2 && count1 > count3)
+            {   
+            txtAnswer1.BackColor = Color.Lime;
+            txtAnswer2.BackColor = Color.Red;
+            txtAnswer3.BackColor = Color.Red;   
+            }
+            if (count2 > count1 && count2 > count3)
+            {
+                txtAnswer1.BackColor = Color.Red;
+                txtAnswer2.BackColor = Color.Lime;
+                txtAnswer3.BackColor = Color.Red;
+            }
+            if (count3 > count1 && count3 > count2)
+            {
+                txtAnswer1.BackColor = Color.Red;
+                txtAnswer2.BackColor = Color.Red;
+                txtAnswer3.BackColor = Color.Lime;
+            }
+            if (count1 > count2 && count1 == count3)
+            {
+                txtAnswer1.BackColor = Color.Lime;
+                txtAnswer2.BackColor = Color.Red;
+                txtAnswer3.BackColor = Color.Lime;
+            }
+            if (count2 > count1 && count2 == count3)
+            {
+                txtAnswer1.BackColor = Color.Red;
+                txtAnswer2.BackColor = Color.Lime;
+                txtAnswer3.BackColor = Color.Lime;
+            }
+            if (count1 > count3 && count1 == count2)
+            {
+                txtAnswer1.BackColor = Color.Lime;
+                txtAnswer2.BackColor = Color.Lime;
+                txtAnswer3.BackColor = Color.Red;
+            }
+        }
+        //Which Ever is Greater Color TextBox In Green ----------------------------------------------------------------------------------------------
+    }
+}
